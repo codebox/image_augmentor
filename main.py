bf3031c77e2d02a204db3ec68635e423643594a2
rob <rob@codebox.org.uk>
2016-09-18 07:49:33 +0100

    adds op handling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys, os, re
 from multiprocessing.dummy import Pool, cpu_count
 from counter import Counter
+from ops.rotate import Rotate
+from skimage.io import imread, imsave
 
 EXTENSIONS = ['png', 'jpg', 'jpeg', 'bmp']
 WORKER_COUNT = max(cpu_count() - 1, 1)
+OPERATIONS = [Rotate]
 
 '''
 Augmented files will have names matching the regex below, eg
@@ -17,22 +20,49 @@ EXTENSION_REGEX = re.compile('|'.join(map(lambda n : '.*\\.' + n + '$', EXTENSIO
 thread_pool = None
 counter = None
 
-def work(d, f):
-    counter.processed()
-    pass
+def build_augmented_file_name(original_name, ops):
+    root, ext = os.path.splitext(original_name)
+    result = root
+    for op in ops:
+        result += '__' + op.code
+    return result + ext
 
-def process(dir, file):
-    thread_pool.apply_async(work, (dir, file))
+def work(d, f, ops):
+    in_path = os.path.join(d,f)
+    for op in ops:
+        img = imread(in_path)
+        img = op.process(img)
+        out_file_name = build_augmented_file_name(f, [op])
+        imsave(os.path.join(d, out_file_name), img)
+
+    counter.processed()
+
+def process(dir, file, ops):
+    thread_pool.apply_async(work, (dir, file, ops))
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'Usage: {} <image directory>'.format(sys.argv[0])
+    if len(sys.argv) < 3:
+        print 'Usage: {} <image directory> <operation> (<operation> ...)'.format(sys.argv[0])
         sys.exit(1)
 
     image_dir = sys.argv[1]
     if not os.path.isdir(image_dir):
         print 'Invalid image directory: {}'.format(image_dir)
         sys.exit(2)
+
+    op_codes = sys.argv[2:]
+    ops = []
+    for op_code in op_codes:
+        op = None
+        for op in OPERATIONS:
+            op = op.match_code(op_code)
+            if op:
+                ops.append(op)
+                break
+
+        if not op:
+            print 'Unknown operation {}'.format(op_code)
+            sys.exit(3)
 
     counter = Counter()
     thread_pool = Pool(WORKER_COUNT)
@@ -48,7 +78,7 @@ if __name__ == '__main__':
                 if AUGMENTED_FILE_REGEX.match(file_name):
                     counter.skipped_augmented()
                 else:
-                    process(dir_name, file_name)
+                    process(dir_name, file_name, ops)
             else:
                 counter.skipped_no_match()
 
